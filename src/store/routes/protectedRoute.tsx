1ed8417b35d258599fc6dcf5f744bf4c13905ceb
maksimprokopec <maksimka.prokopets@mail.ru>
2024-08-28 22:17:29 +0700

    Исправлен баг с обновлением страницы Это сломается, неизвестно когда, но это сломается, переделать

--- src/store/routes/protectedRoute.tsx
+++ src/store/routes/protectedRoute.tsx
@@ -1,13 +1,26 @@
 import { Navigate, Outlet } from 'react-router-dom';
 import { useSelector } from 'react-redux';
-import { RootState } from '../store.ts';
+import { RootState } from '../store/store.ts';
+import { useEffect, useState } from 'react';
 
 interface ProtectedRouteProps {
     allowedRoles: number[];
 }
 
 const ProtectedRoute = ({ allowedRoles }: ProtectedRouteProps) => {
+    const [isLoading, setIsLoading] = useState(true);
     const user = useSelector((state: RootState) => state.auth.user);
+
+    useEffect(() => {
+
+        const timer = setTimeout(() => setIsLoading(false), 20);
+        return () => clearTimeout(timer);
+    }, []);
+
+    if (isLoading) {
+
+        return <div>Loading...</div>;
+    }
 
     if (!user) {
         return <Navigate to="/" replace />;
