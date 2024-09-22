858fa85932799f7a7526e76f252c0b8142891e61
maksimprokopec <maksimka.prokopets@mail.ru>
2024-09-23 01:25:18 +0700

    Отображение по стандарту

--- src/utils/phoneNumber.ts
+++ src/utils/phoneNumber.ts
@@ -16,11 +16,11 @@ export const formatPhoneNumberOnInput = (phoneNumber: string): string => {
     } else if (cleaned.length <= 4) {
         return `${cleaned[0]}-(${cleaned.slice(1)}`; // Форматируем как "8-XXX"
     } else if (cleaned.length <= 7) {
-        return `${cleaned[0]}-(${cleaned.slice(1, 4)})-${cleaned.slice(4)}`; // Форматируем как "8-XXX-(XXX"
+        return `${cleaned[0]}-(${cleaned.slice(1, 4)})-${cleaned.slice(4)}`; // Форматируем как "8-(XXX)-XXX"
     } else if (cleaned.length <= 9) {
-        return `${cleaned[0]}-(${cleaned.slice(1, 4)})-${cleaned.slice(4, 7)}-${cleaned.slice(7)}`; // "8-XXX-(XXX)-XX"
+        return `${cleaned[0]}-(${cleaned.slice(1, 4)})-${cleaned.slice(4, 7)}-${cleaned.slice(7)}`; // "8-(XXX)-XXX-XX"
     } else if (cleaned.length <= 11) {
-        return `${cleaned[0]}-(${cleaned.slice(1, 4)})-${cleaned.slice(4, 7)}-${cleaned.slice(7, 9)}-${cleaned.slice(9)}`; // Полное форматирование "8-XXX-(XXX)-XX-XX"
+        return `${cleaned[0]}-(${cleaned.slice(1, 4)})-${cleaned.slice(4, 7)}-${cleaned.slice(7, 9)}-${cleaned.slice(9)}`; // Полное форматирование "8-(XXX)-XXX-XX-XX"
     }
 
     return phoneNumber;
