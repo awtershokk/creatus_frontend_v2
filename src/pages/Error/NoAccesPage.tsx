0b0b7beaafce7ea90be1e5c91846de4e0762bb43
maksimprokopec <maksimka.prokopets@mail.ru>
2024-09-25 16:06:00 +0700

    Стилизация страниц error

--- src/pages/Error/NoAccesPage.tsx
+++ src/pages/Error/NoAccesPage.tsx
@@ -2,7 +2,7 @@ import BlueLink from "../../components/Text/BlueLink.tsx";
 
 const NoAccessPage = () => {
     return (
-        <div className="flex items-center justify-center h-screen bg-gray-100">
+        <div className="flex items-center justify-center w-screen h-screen bg-gray-100">
             <div className="bg-white p-6 rounded shadow-md w-80 text-center">
                 <h2 className="text-2xl font-bold  text-black">Нет доступа</h2>
                 <p className="text-black">У вас нет прав на просмотр данной страницы.</p>
