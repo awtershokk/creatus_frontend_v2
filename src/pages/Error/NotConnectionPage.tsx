2514244e7a6a9624008e4cd22f9d62591fc6b94e
Grisha Kuznetsov <126278966+awtershokk@users.noreply.github.com>
2024-10-02 15:09:03 +0700

    изменение дизайна в перезагрузке

--- src/pages/Error/NotConnectionPage.tsx
+++ src/pages/Error/NotConnectionPage.tsx
@@ -1,9 +1,15 @@
-import React from 'react';
-import not_connection from '../../assets/not_connection.gif';
+import React, { useState } from 'react';
+import { VscDebugDisconnect } from "react-icons/vsc";
+import { AiOutlineLoading3Quarters } from "react-icons/ai";
 
 function NotConnectionPage() {
+    const [isLoading, setIsLoading] = useState(false);
+
     const handleRefresh = () => {
-        window.location.reload();
+        setIsLoading(true);
+        setTimeout(() => {
+            window.location.reload();
+        }, 2000);
     };
 
     return (
@@ -26,8 +32,19 @@ function NotConnectionPage() {
                 </p>
                 <button
                     onClick={handleRefresh}
-                    className="bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-700 transition duration-300">
-                    Проверить подключение
+                    className={`p-2 bg-gray-700 text-white rounded-full transition ml-1 text-s ${
+                        isLoading ? 'cursor-not-allowed opacity-50' : 'hover:bg-gray-900'
+                    }`}
+                    disabled={isLoading}
+                >
+                    {isLoading ? (
+                        <AiOutlineLoading3Quarters className="animate-spin text-white mr-2" />
+                    ) : (
+                        <VscDebugDisconnect className="text-white mr-0.5 ml-0.5" />
+                    )}
+                    <span className="text-white">
+                        {isLoading ? 'Перезагрузка...' : 'Проверить подключение'}
+                    </span>
                 </button>
             </div>
         </div>
