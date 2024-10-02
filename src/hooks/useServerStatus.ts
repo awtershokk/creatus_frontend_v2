33cbef88fe7832c02d00151b7a61c954c5c3c599
maksimprokopec <maksimka.prokopets@mail.ru>
2024-10-02 12:52:19 +0700

    Проверка бека на вшивость

---
+++ src/hooks/useServerStatus.ts
@@ -0,0 +1,52 @@
+
+import { useState, useEffect } from 'react';
+import { fetchBuilding } from '../api/requests/buildingApi.ts';
+
+const useServerStatus = (onConnectionRestored) => {
+    const [serverConnected, setServerConnected] = useState(true);
+    const [prevServerConnected, setPrevServerConnected] = useState(true);
+
+    const checkServerConnection = async () => {
+        try {
+            const building = await fetchBuilding(1);
+            setServerConnected(!!building);
+        } catch (error) {
+            console.error('Ошибка подключения к серверу:', error);
+            setServerConnected(false);
+        }
+    };
+
+    useEffect(() => {
+
+        checkServerConnection();
+
+        const intervalId = setInterval(() => {
+            checkServerConnection();
+        }, 20000);
+
+        return () => clearInterval(intervalId);
+    }, []);
+
+    useEffect(() => {
+
+        if (!prevServerConnected && serverConnected) {
+            onConnectionRestored();
+        }
+
+
+        if (!serverConnected) {
+            const reloadInterval = setInterval(() => {
+                console.log('Перезагрузка страницы из-за потери соединения...');
+                window.location.reload();
+            }, 20000);
+
+            return () => clearInterval(reloadInterval);
+        }
+
+        setPrevServerConnected(serverConnected);
+    }, [serverConnected, prevServerConnected, onConnectionRestored]);
+
+    return { serverConnected, checkServerConnection };
+};
+
+export default useServerStatus;
